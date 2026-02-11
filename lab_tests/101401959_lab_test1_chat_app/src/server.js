a711566b47b90ce8b4045f6e9bfed4afb20efb78
HansonBK <hansonhans88@gmail.com>
2026-02-11 15:47:22 -0500

    Setup MongoDB connection and Socket.io server

--- lab_tests/101401959_lab_test1_chat_app/src/server.js
+++ lab_tests/101401959_lab_test1_chat_app/src/server.js
@@ -1,13 +1,44 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const http = require("http");
+const { Server } = require("socket.io");
+const morgan = require("morgan");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  res.send("Chat server is running ✅");
+  res.send("Chat Server Running ✅");
 });
 
+// create server for socket.io
+const server = http.createServer(app);
+
+// socket.io setup
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+// socket test
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+// connect MongoDB
+connectDB();
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
