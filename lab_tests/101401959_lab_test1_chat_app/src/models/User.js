87d61b5d6443b3bf3dec7b78d90d6fefe828dcca
HansonBK <hansonhans88@gmail.com>
2026-02-11 15:48:56 -0500

    Add Mongoose models for users and chat messages

---
+++ lab_tests/101401959_lab_test1_chat_app/src/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: true, trim: true },
+    firstname: { type: String, required: true, trim: true },
+    lastname: { type: String, required: true, trim: true },
+    password: { type: String, required: true },
+    createdon: { type: String, required: true },
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model("User", userSchema);
